ca49853f29d8b84954ae9e739c7d0f799e92681e
yousefsalem <yousef_salem97@yahoo.com>
2022-11-04 22:35:12 -0500

    fix supplier

---
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/dao/ProductDAO.java
@@ -0,0 +1,11 @@
+//package com.hackerrank.eshopping.product.dashboard.dao;
+//
+//import com.hackerrank.eshopping.product.dashboard.model.Product;
+//
+//import java.util.List;
+//
+//public class ProductDAO {
+//    public List<Product> findAllByCategoryOrderByAvailabilityDescDiscountedPriceAsc(String category);
+//
+//    public List<Product> findAllByCategoryAndAvailability(String category, boolean availability);
+//}
